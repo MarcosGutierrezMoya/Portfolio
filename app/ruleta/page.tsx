59922c963baa24bb06ce0a4d51308d4bb7fab552
MarcosGutierrezMoya <marcos.gutierrez.moya2010@gmail.com>
2023-11-26 16:48:19 +0100

    cambio lógica seteo data

--- app/ruleta/page.tsx
+++ app/ruleta/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useEffect, useState } from "react";
 import Regalos from "./Regalos";
-import { getFamilyData } from "../_componentes/web/webData";
+import { getFamilyData, setFamilyData } from "../_componentes/web/webData";
 import Link from "next/link";
 
 type Persona = {
@@ -13,37 +13,17 @@ type Persona = {
 
 const Ruleta = () => {
 
-    const [yaElegidos, setYaElegidos] = useState<Persona[]>([]);
     const [participantes, setParticipantes] = useState<Persona[]>([]);
     const [nombre, setNombre] = useState<Persona>();
     const [nombreAmigo, setNombreAmigo] = useState<Persona>();
 
-    function random(familia: Persona[] | any, e: any) {
+    function random(e: any) {
         const rawNombre = participantes.find(el => el.nombre === e.target.value)
-        const arrayFamilia = [] as Persona[];
-        familia.forEach((element: Persona) => {
-            if (element.leRegala == "" && element.nombre !== rawNombre?.nombre) {
-                arrayFamilia.push(element);
-            }
-            else {
 
-            }
-        });
-
-        setParticipantes(arrayFamilia);
         setNombre(rawNombre)
         mostrarRegalos(rawNombre?.aRegalar||"");
         localStorage.setItem("yo",JSON.stringify(rawNombre));
 
-    }
-
-    function setChoosenOne() {
-        if (yaElegidos.length < 1) {
-            const rdm = Math.floor(Math.random() * participantes.length);
-            const nombre = participantes.splice(rdm, 1);
-
-            setYaElegidos(nombre);
-        }
     }
 
     function mostrarRegalos(nombreDeAmigo:string) {
@@ -64,7 +44,7 @@ const Ruleta = () => {
             }
             const nuevoYo = participantes.find(p=>p.nombre === yo.nombre);
             if (nuevoYo?.aRegalar !== "") {
-                setNombre(yo);         
+                setNombre(nuevoYo);         
                 
                 mostrarRegalos(yo.aRegalar);
             }
@@ -84,22 +64,13 @@ const Ruleta = () => {
             {nombre !== undefined ?
                 <div>
                     <p className="text-[1.5rem] md:sm:text-[2rem] md:xl:text-[3rem] text-center pb-6">Eres {nombre?.nombre}</p>
-                    {nombre.aRegalar === "" ?
-                        <section className="flex flex-col gap-4 md:sm:flex-row justify-around p-8 w-full">
-                            <div className='flex justify-center items-center h-full'>
-                                {yaElegidos?.length != 0 ?
-                                    <p className='text-[1.5rem] md:sm:text-2xl'>Te ha tocado... {yaElegidos[0]?.nombre}</p>
-                                    :
-                                    <button onClick={setChoosenOne} className='hover:bg-green-900/25 border-green-500 border-2 border-double text-[1.5rem] md:sm:text-[2.5rem] p-2'>Ver amigo invisible</button>
-                                }
-                            </div>
-                            <Regalos registrado={nombre} regala={yaElegidos[0]?.nombre} />
-                        </section>
+                    {nombre.aRegalar === "" ?                        
+                            <Regalos registrado={nombre} participantes={participantes} />
                         :
                         <div className="flex flex-col items-center justify-center gap-6 w-screen">
                             <Link href={"/ruleta/pedidos"} className='px-4 py-2 border-2 border-green-500 bg-blue-900/25 hover:bg-blue-900/75'>Cambiar tus regalos</Link>
                             <p className="md:sm:pt-0 text-center p-2 text-[1.8rem] md:sm:text-[2.2rem]">Tu amigo invisible es {nombre.aRegalar}</p>
-                            <ol className='list-decimal w-fit'>{nombreAmigo?.regalos.map((present, i) => {
+                            <ol className='list-decimal w-fit p-2'>{nombreAmigo?.regalos.map((present, i) => {
                                 return (
                                     <li key={present + i} className='flex justify-between text-[1.5rem] md:sm:text-[2rem]'>{present.includes("http") ? <a className='underline cursor-pointer text-[0.5rem] md:sm:text-[1rem] text-sky-500 w-screen p-4' href={present} target='_blank'>{i+1}. {present}</a> : <p>{i+1}. {present}</p>}</li>
                                 )
@@ -108,8 +79,8 @@ const Ruleta = () => {
                     }
                 </div>
                 :
-                <select onChange={(e) => { random(participantes, e) }} className="w-[75%] p-2 text-[0.5rem] md:sm:w-[50%] md:sm:text-[1rem]">
-                    <option value="escoge">{participantes.length!==0?"Dime cuál es tu nombre":"No está conectando a la base de datos"}</option>
+                <select onChange={(e) => { random(e) }} className="w-[75%] p-2 text-[0.8rem] md:sm:w-[50%] md:sm:text-[1.2rem]">
+                    <option value="escoge">"Dime cuál es tu nombre"</option>
                     {participantes?.map((persona: Persona) => {
                         return (
                             <option key={persona.nombre} value={persona.nombre}>{persona.nombre}</option>
